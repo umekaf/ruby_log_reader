41a49a3f713be402dd43c8392c964597858bb096
umekaf <kafleumesh@gmail.com>
2022-04-11 01:41:41 +0200

    Added file to group the information

--- lib/log_reader.rb
+++ lib/log_reader.rb
@@ -5,6 +5,7 @@ require 'log_reader/file_reader'
 require 'log_reader/line_content_segregator'
 require 'log_reader/content_segregator'
 require 'log_reader/aggregator'
+require 'log_reader/collector'
 
 module LogReader
   class Error < StandardError; end
---
+++ lib/log_reader/collector.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+module LogReader
+  class Collector
+    def initialize(content); end
+
+    def group; end
+  end
+end
