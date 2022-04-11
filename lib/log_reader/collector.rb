b24a519c0647b737b48d4e4ef0faac3a278c6f06
umekaf <kafleumesh@gmail.com>
2022-04-11 03:28:00 +0200

    Added logic for the reader

--- lib/log_reader/collector.rb
+++ lib/log_reader/collector.rb
@@ -9,7 +9,7 @@ module LogReader
     attr_reader :contents
 
     def group
-      contents.each_with_object({}) do |content, collection|
+      contents.each_with_object(SortCollection.new) do |content, collection|
         collection[content.path] ||= Aggregator.new
         collection[content.path].call(content)
       end
--- lib/log_reader/reader.rb
+++ lib/log_reader/reader.rb
@@ -2,8 +2,28 @@
 
 module LogReader
   class Reader
-    def initialize(file_path); end
+    def initialize(file_path)
+      @file_path = file_path
+    end
 
-    def format; end
+    attr_reader :file_path
+
+    def format
+      ResponseFormatter.new(collector_data).format
+    end
+
+    private
+
+    def collector_data
+      Collector.new(segregated_data).group
+    end
+
+    def segregated_data
+      ContentSegregator.new(read_data).segregate
+    end
+
+    def read_data
+      FileReader.new(file_path).file_lines
+    end
   end
 end
